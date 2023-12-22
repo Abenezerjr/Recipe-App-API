a248f492daad75f6035ffb939473af4532318e2b
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-12-22 12:08:11 +0300

    Django configured

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,3 +1,12 @@
+ 
+def add(x,y):
+
+  return x+y
+
+def subtract(x,y):
+
+  return y-x
+
 
  
  
--- app/app/test.py
+++ app/app/test.py
@@ -1,1 +1,11 @@
+from django.test import SimpleTestCase
+from app import calc
 
+class CalcTests(SimpleTestCase):
+  def test_add_number(self):
+    res=calc.add(5, 6)
+    self.assertAlmostEqual(res, 11)
+
+  def test_subtract_numbers(self):
+    res=calc.subtract(10, 15)
+    self.assertAlmostEqual(res, 5)
