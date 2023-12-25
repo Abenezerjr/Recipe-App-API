4db904869bbef49cd0f65cd6232eefcb598398af
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-12-25 10:47:53 +0300

    setup the Admin Page

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -29,9 +29,8 @@ class ModelTest(TestCase):
             ['test4@example.COM', 'test4@example.com'],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email,'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
-
 
     def test_new_user_without_email_riases_error(self):
         """Test that creating a user without an email raises a  ValueError"""
