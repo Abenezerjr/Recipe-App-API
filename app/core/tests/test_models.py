922aa918f3b10f74154e91116c49d298d92db2f6
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2024-01-04 02:24:24 +0300

    Implement Tag Api and debug user authentication Token

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(email='user@exaple.com', password='testpass123'):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """test model"""
 
@@ -67,3 +72,10 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_creat_tag(self):
+        """test crating a tag i successful."""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
